6de5db87d3d79725066037ea958d828ed4b4594b
Andrew Shillingford <ashillin@pratt.edu>
2026-05-04 17:40:54 -0400

    Created Site's show page

--- routes/sites.js
+++ routes/sites.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Site = require('../models/site');
+const Volunteer = require('../models/volunteer');
 
 router.get('/', function(req, res, next) {
     const sites = Site.all; 
@@ -8,7 +9,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/form', async (req, res, next) => {
-  res.render('sites/form', { title: 'VolunteerCenter || Sites', site: site });
+  res.render('sites/form', { title: 'VolunteerCenter || Sites', site: site, volunteers: Volunteer.all });
 });
 
 router.post('/upsert', async (req, res, next) => {
@@ -20,7 +21,18 @@ router.post('/upsert', async (req, res, next) => {
 router.get('/edit', async (req, res, next) => {
   let siteIndex = req.query.id;
   let site = Site.get(siteIndex);
-  res.render('sites/form', { title: 'VolunteerCenter || Sites', site: site, siteIndex: siteIndex  });
+  res.render('sites/form', { title: 'VolunteerCenter || Sites', site: site, siteIndex: siteIndex, volunteers: Volunteer.all  });
+});
+
+router.get('/show/:id', async (req, res, next) => {
+  let templateVars = {
+    title: 'VolunteerCenter || Sites',
+    site: Site.get(req.params.id)
+  }
+if (templateVars.site.volunteerIndex) {
+    templateVars.volunteers = templateVars.site.volunteerIndex.map((volunteerIndex) => Volunteer.get(volunteerIndex));
+  }
+  res.render('sites/show', templateVars);
 });
 
 module.exports = router;
