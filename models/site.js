b39aafb93a764e3168bf3457124e00759e72ec94
Andrew Shillingford <ashillin@pratt.edu>
2026-05-04 18:04:39 -0400

    handlebar helpers i guess

--- models/site.js
+++ models/site.js
@@ -1,7 +1,7 @@
   const sites = [
-    {siteName: "Valentine Ave", siteAddy: "2544 Valentine"}, 
-    {siteName: "University Heights", siteAddy: "149th street 3rd Ave"}, 
-    {siteName: "Fordham University", siteAddy: "Fordham Road"},
+    {siteName: "Valentine Ave", siteAddy: "2544 Valentine", volunteerIndex: ["0"]}, 
+    {siteName: "University Heights", siteAddy: "149th street 3rd Ave", volunteerIndex: ["1"]}, 
+    {siteName: "Fordham University", siteAddy: "Fordham Road", volunteerIndex: ["2"]},
   ];
 
 exports.add = (site) => {
--- volunteerCenter.js
+++ volunteerCenter.js
@@ -11,7 +11,26 @@ const sitesRouter = require('./routes/sites');
 //Framework Setup
 const app = express();
 const port = 3000;
-const handlebars = require('express-handlebars').create();
+
+var handlebars = require('express-handlebars').create({
+  helpers: {
+    eq: (v1, v2) => v1 == v2,
+    ne: (v1, v2) => v1 != v2,
+    lt: (v1, v2) => v1 < v2,
+    gt: (v1, v2) => v1 > v2,
+    lte: (v1, v2) => v1 <= v2,
+    gte: (v1, v2) => v1 >= v2,
+    and() {
+        return Array.prototype.every.call(arguments, Boolean);
+    },
+    or() {
+        return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+    },
+    someId: (arr, id) => arr && arr.some(obj => obj.id == id),
+    in: (arr, obj) => arr && arr.some(val => val == obj),
+    dateStr: (v) => v && v.toLocaleDateString("en-US")
+  }
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true }));
